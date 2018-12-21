e7a61063eb66875338c089aba4302cf6c73c0421
alicemitali <alicemitali6@gmail.com>
2018-12-21 12:12:48 +0200

    message

--- quiz/js/script.js
+++ quiz/js/script.js
@@ -1,2 +1,7 @@
 var answers = ["D", "A", "C", "A", "C", "B", "D", "A", "D", "B"],
   tot = answers.length;
+  function getCheckedValue(radioName) {
+    var radios = document.getElementsByName(radioName); // Get radio group by-name
+    for (var y = 0; y < radios.length; y++) {
+        if (radios[y].checked) return radios[y].value; // return the checked value
+    }
